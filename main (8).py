95799e193b284076df83b140d7b3d2e8779de09f
Asmku637c2s41601 <144544604+Asmku637c2s41601@users.noreply.github.com>
2023-10-16 20:02:18 +0530

    Add files via upload

---
+++ main (8).py
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+class student:
+
+  def __init__(self, name, roll_number, cgpa):
+    self.name = name
+    self.roll_number = roll_number
+    self.cgpa = cgpa
+
+
+def sort_students(student_list):
+
+    sorted_students =  sorted(student_list,key=lambda student: student.cgpa, reverse=True)
+
+
+    return sorted_students
+
+
+
+students = [
+  student("abarna", "A123", 9.5),
+  student("ani", "A124", 9.4),
+  student("divya", "A125", 9.3),
+  student("vishali", "A126", 9.2),
+]
+
+
+sorted_students =sort_students(students)
+
+
+for student in sorted_students:
+  print("Name: {}, Roll Number: {}, CGPA: {}".format(student.name, student.roll_number, student.cgpa )) 
+    
